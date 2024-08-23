3cf362a6472caf95f02c7db80c928e1bcdeacc9b
cinmon <gianellaalessandrac@gmail.com>
2024-08-23 19:06:05 +0900

    Complete repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if(num < 0 ) {
+        return "ERROR";
+    }
+
+    let return_string = "";
+
+    for(let i = 0 ; i < num ; i++) {
+        return_string += string;
+    }
+
+    return return_string
 
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
