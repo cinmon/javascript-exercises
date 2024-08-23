d702ffee0d85297e17aae18eff299cd35597a5c5
cinmon <gianellaalessandrac@gmail.com>
2024-08-23 22:10:15 +0900

    Complete sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let sum = 0;
+
+    if(num1 < 0 || num2 < 0) {
+        // returns error for negative values
+        return "ERROR";
+    }
+    else if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        // Checks if the number is an integer, and, furthermore, if it's a number
+        return "ERROR";
+    }
+
+    if(num1 > num2) {
+        // num1 is always the smallest
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    for(let i = num1 ; i <= num2 ; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
