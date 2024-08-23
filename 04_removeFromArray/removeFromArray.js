00b7950e2cf2b7f8aaf6996ab68709617d6ca09a
cinmon <gianellaalessandrac@gmail.com>
2024-08-23 21:35:49 +0900

    Complete removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rest) {
+
+    let modifiedArray = array.map((x) => x);
+
+    outer:
+    for(let i = 0 ; i < modifiedArray.length ; ) {
+        for(let j = 0 ; j < rest.length ; j++) {
+            if(modifiedArray[i] === rest[j]) {
+                modifiedArray.splice(i, 1);
+                continue outer;
+            }
+        }
+        i++;
+    }
+
+    return modifiedArray; // modified array
+
 };
 
 // Do not edit below this line
