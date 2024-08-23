b1b9188f90b1d14538055f68e7061b8340c03fe2
cinmon <gianellaalessandrac@gmail.com>
2024-08-23 22:34:31 +0900

    Complete temp conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far_degrees) {
+
+  let cel_degrees = (far_degrees - 32) * (5/9);
+  return Math.round(cel_degrees * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel_degrees) {
+
+  let far_degrees = cel_degrees * (9/5) + 32;
+  return Math.round(far_degrees * 10) / 10;     // Rounds to 1 decimal
+
 };
 
 // Do not edit below this line
